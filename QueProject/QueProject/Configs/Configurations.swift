147397a8f541575810ef20ecaf9bfdc6535f6c9e
TrongQue <dinhtrongque01071996@gmail.com>
2025-02-18 09:58:45 +0700

    add baseURL into xcconfig

--- QueProject/QueProject/Configs/Configurations.swift
+++ QueProject/QueProject/Configs/Configurations.swift
@@ -9,12 +9,9 @@ import Foundation
 
 struct APIConfig {
     static var baseURL: String {
-        #if DEV
-            return "https://api.github.dev.com"
-        #elseif STG
-            return "https://api.github.stg.com"
-        #else
-            return "https://api.github.com"
-        #endif
+        guard let baseURL = Bundle.main.object(forInfoDictionaryKey: "BaseURL") as? String else {
+            fatalError("BaseURL is not set in Info")
+        }
+        return baseURL
     }
 }
