63b7b974c9f9023d6f2b7f31ce62ee296a79f166
TrongQue <dinhtrongque01071996@gmail.com>
2025-02-18 16:08:32 +0700

    use BundleProperty

--- QueProject/QueProject/Configs/Configurations.swift
+++ QueProject/QueProject/Configs/Configurations.swift
@@ -7,11 +7,29 @@
 
 import Foundation
 
+@propertyWrapper
+struct BundleProperty<T> {
+    private let key: String
+    
+    init(key: String) {
+        self.key = key
+    }
+    
+    var wrappedValue: T {
+        guard let value = Bundle.main.infoDictionary?[key] as? T else {
+            fatalError("Key \(key) is not set in Info.plist")
+        }
+        return value
+    }
+}
+
+extension Bundle {
+    @BundleProperty(key: "BaseURL")
+    static var baseURL: String
+}
+
 struct APIConfig {
     static var baseURL: String {
-        guard let baseURL = Bundle.main.object(forInfoDictionaryKey: "BaseURL") as? String else {
-            fatalError("BaseURL is not set in Info")
-        }
-        return baseURL
+        Bundle.baseURL
     }
 }
